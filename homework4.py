f87cdf0b4184b88561efe9b0e3d4460425b803ee
biZ0Zon <gos85@inbox.ru>
2024-07-15 02:21:55 +0300

    Delete homework4.py

--- homework4.py
+++
@@ -1,7 +0,0 @@
-my_string = input("Введите ФИО и нажмите Enter: ")
-print(my_string)
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[:1])
-print(my_string[-1:])
